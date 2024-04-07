8ea5dfdd7525f4250c468c8c270fab71c27489ad
Nargis <nargissamatova16@gmail.com>
2024-04-07 21:10:51 +0300

    lists and keys task completed

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,10 +4,36 @@ import List from "./Lists";
 import fruits from "./Lists";
 
 function App() {
+  const fruits = [
+    { name: "apple", calories: 52 },
+    { name: "banana", calories: 89 },
+    { name: "orange", calories: 47 },
+    { name: "strawberry", calories: 33 },
+    { name: "grape", calories: 69 },
+    { name: "watermelon", calories: 30 },
+    { name: "pineapple", calories: 50 },
+    { name: "kiwi", calories: 61 },
+    { name: "mango", calories: 60 },
+    { name: "peach", calories: 39 },
+  ];
+  const vegetables = [
+    { name: "carrot", calories: 41 },
+    { name: "broccoli", calories: 34 },
+    { name: "spinach", calories: 23 },
+    { name: "cucumber", calories: 15 },
+    { name: "bell pepper", calories: 31 },
+    { name: "tomato", calories: 18 },
+    { name: "onion", calories: 40 },
+    { name: "zucchini", calories: 17 },
+    { name: "celery", calories: 6 },
+    { name: "lettuce", calories: 5 },
+  ];
+
   return (
     <>
       <Greeting isLoggedIn={true} name="Nargis" />
-      <List />
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
     </>
   );
 }
--- counter/src/Lists.jsx
+++ counter/src/Lists.jsx
@@ -1,25 +1,14 @@
 // RENDER LISTS
 
-function List() {
-  const fruits = [
-    { name: "apple", calories: 52 },
-    { name: "banana", calories: 89 },
-    { name: "orange", calories: 47 },
-    { name: "strawberry", calories: 33 },
-    { name: "grape", calories: 69 },
-    { name: "watermelon", calories: 30 },
-    { name: "pineapple", calories: 50 },
-    { name: "kiwi", calories: 61 },
-    { name: "mango", calories: 60 },
-    { name: "peach", calories: 39 },
-  ];
+function List(props) {
+  const itemList = props.items;
 
   // SORTING
-  fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL ORDER
-  fruits.sort((a, b) => a.calories - b.calories); // NUMERIC ORDER
+  itemList.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL ORDER
+  itemList.sort((a, b) => a.calories - b.calories); // NUMERIC ORDER
 
   // FILTERING LOW CALORIES FRUITS
-  const lowCalFruits = fruits.filter((fruit) => fruit.calories < 50);
+  const lowCalFruits = itemList.filter((item) => item.calories < 50);
 
   const listItem = lowCalFruits.map((lowCalfruit, index) => (
     <li key={index}>
