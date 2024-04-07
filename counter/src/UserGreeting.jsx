5a78bba9cb6ac2c3e6a5685f43d1bf97adae71e9
Nargis <nargissamatova16@gmail.com>
2024-04-07 17:49:20 +0300

    user greeting task completed

--- counter/src/UserGreeting.jsx
+++ counter/src/UserGreeting.jsx
@@ -1,8 +1,9 @@
 function Greeting(props) {
-  return props.isLoggedIn ? (
-    <h2>Welcome {props.name}</h2>
-  ) : (
-    <h2>You should login first</h2>
+  const welcomeMessage = (
+    <h2 className="welcome-message">Welcome {props.name}</h2>
   );
+  const loginPrompt = <h2 className="login-prompt">You should login first</h2>;
+
+  return props.isLoggedIn ? welcomeMessage : loginPrompt;
 }
 export default Greeting;
