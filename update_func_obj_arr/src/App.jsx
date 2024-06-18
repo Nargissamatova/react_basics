fe631fe818bfc306836284ed9e53eb4ef2183b08
Nargis <nargissamatova16@gmail.com>
2024-06-18 19:57:00 +0300

    Add array uppdater

--- update_func_obj_arr/src/App.jsx
+++ update_func_obj_arr/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Counter from "./Counter";
 import UpdateObject from "./UpdateObject";
+import UpdateArray from "./UpdateArray";
 
 function App() {
   return (
     <>
       <Counter />
       <UpdateObject />
+      <UpdateArray />
     </>
   );
 }
---
+++ update_func_obj_arr/src/UpdateArray.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function UpdateArray() {
+  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+
+  function handleAddFood() {
+    const newFood = document.getElementById("foodInput").value;
+    document.getElementById("foodInput").value = "";
+    // spread operator
+    setFoods((f) => [...f, newFood]);
+  }
+  function handleRemoveFood(index) {
+    // Filter all elements where current i(index) does not equal the index that we receive
+    // setFoods(foods.filter((element, i) => i !== index)); element is ignored, so undescore is used
+    setFoods(foods.filter((_, i) => i !== index));
+  }
+
+  return (
+    <div>
+      <h2>List of foods</h2>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index} onClick={() => handleRemoveFood(index)}>
+            {food}
+          </li>
+        ))}
+      </ul>
+      <input type="text" id="foodInput" placeholder="Enter food name" />
+      <button onClick={handleAddFood}>Add food</button>
+    </div>
+  );
+}
+
+export default UpdateArray;
